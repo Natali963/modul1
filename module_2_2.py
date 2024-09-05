dddf1efff23ec409645dfe16a0968cf18cb607b8
Natali963 <Natty_info@mail.ru>
2024-09-05 16:17:30 +0300

    Условная конструкция (операторы if, elif, else)

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+first = input()
+second = input()
+third = input()
+#print(f'{first} {second} {third}') - это для себя!
+
+if first == second ==third:
+    print(3)
+elif first==second!=third or first!=second ==third or first==third!=second:
+    print(2)
+else:
+    print(0)
