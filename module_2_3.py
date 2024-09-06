70217118c97897c09cf3be9da46c8fa608ee33ed
Natali963 <Natty_info@mail.ru>
2024-09-07 01:33:19 +0300

    цикл while

---
+++ module_2_3.py
@@ -0,0 +1,21 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+l = my_list
+print(l)
+a = len(l)
+print(a)
+i = 0
+
+
+while i < a:
+    if l [i] <= 0:
+        i += 1
+        break
+    print(l[i])
+    i += 1
+    
+     
+
+
+
+
+
