8f4de0c06dd6e82f2fe3f829c0fa9ba8052d4a62
Natali963 <Natty_info@mail.ru>
2024-09-09 08:55:14 +0300

    цикл while (с корректировкой2)

--- module_2_3.py
+++ module_2_3.py
@@ -1,12 +1,10 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 l = my_list
-print(l)
 a = len(l)
-print(a)
 i = 0
 
 
-while i < a:
+while i <= a:
     if l [i] <= 0:
         i += 1
         continue
@@ -15,8 +13,7 @@ while i < a:
     if l [i] <= 0:
         i += 1
         break
-    print(l[i])
-    i += 1
+print(l[i])
 
     
      
